00aaebee121559171889baa96e9ea2e565131ec3
suphakrit <suphakrit_kongkham@cmu.ac.th>
2022-12-13 13:06:49 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int odd = 0;
+    int even = 0;
+    int num ;
+
+    while(num != 0){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if(num % 2 == 0){
+            even = even + 1;
+        }
+        else{
+            odd = odd + 1;
+        }
+
+    }
+    cout << "#Even numbers = " <<even<<"\n";
+    cout << "#Odd numbers = "<<odd-1;
+
+
     return 0;
 }
